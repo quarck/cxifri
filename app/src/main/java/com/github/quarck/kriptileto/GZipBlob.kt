ba38877f51550c16c56b0b3b213a584471345470
Sergey Parshin <quarck@gmail.com>
2018-10-16 17:49:56 +0100

    GZipBlob added

---
+++ app/src/main/java/com/github/quarck/kriptileto/GZipBlob.kt
@@ -0,0 +1,28 @@
+package com.github.quarck.kriptileto
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
+
+class GZipBlob {
+    fun deflate(inp: ByteArray): ByteArray {
+        val byteStream = ByteArrayOutputStream()
+        val gzipStream = GZIPOutputStream(byteStream)
+        gzipStream.write(inp, 0, inp.size)
+        gzipStream.finish()
+        val ret = byteStream.toByteArray()
+        gzipStream.close()
+        byteStream.close()
+        return ret
+    }
+
+    fun inflate(inp: ByteArray): ByteArray? {
+        val byteStream = ByteArrayInputStream(inp)
+        val gzipStream = GZIPInputStream(byteStream)
+        val blob = gzipStream.readBytes()
+        gzipStream.close()
+        byteStream.close()
+        return blob
+    }
+}
---
+++ app/src/test/java/com/github/quarck/kriptileto/GZipUnitTests.kt
@@ -0,0 +1,31 @@
+package com.github.quarck.kriptileto
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class GZipUnitTests {
+    @Test
+    fun gzipIsCorrect() {
+        val input = byteArrayOf(1, 2, 3, 4, 5)
+
+        val gz = GZipBlob()
+
+        val output = gz.deflate(input)
+
+        val inputBack = gz.inflate(output)
+        assertNotNull(inputBack)
+
+        if (inputBack != null) {
+            assertEquals(input.size, inputBack.size)
+            for (i in 0 until input.size) {
+                assertEquals(input[i], inputBack[i])
+            }
+        }
+    }
+}
