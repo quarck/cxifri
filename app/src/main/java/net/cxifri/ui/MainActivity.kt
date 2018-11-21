2f19c4aa05f82fc6847090ab2a00701ccb575119
Sergey Parshin <quarck@gmail.com>
2018-11-21 14:14:34 +0000

    Code refactoring

--- app/src/main/java/net/cxifri/ui/MainActivity.kt
+++ app/src/main/java/net/cxifri/ui/MainActivity.kt
@@ -31,11 +31,10 @@ import android.view.WindowManager
 import android.widget.*
 import net.cxifri.R
 import net.cxifri.crypto.*
-import net.cxifri.keysdb.KeysDatabase
 import net.cxifri.utils.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
 
     val buttonEncrypt by UIItem<Button>(R.id.buttonEncrypt)
     val buttonDecrypt by UIItem<Button>(R.id.buttonDecrypt)
@@ -43,21 +42,20 @@ class MainActivity : AppCompatActivity() {
     val messageText by UIItem<EditText>(R.id.message)
     val buttonKeySelect by UIItem<Button>(R.id.buttonKeySelect)
     val passwordText by UIItem<EditText>(R.id.password)
-    val textViewError by UIItem<TextView>(R.id.textViewError)
 
     val pleaseWaitText by UIItem<TextView>(R.id.textDerivingKeysStatus)
 
     var isTextEncrypted = false
 
-    var isKeyEverSelected = false
-
-    var currentKey: KeyEntry? = null
+    lateinit var controller: MainActivityController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        controller = MainActivityController(context = this, view = this)
 
         buttonKeySelect.setOnClickListener(this::onButtonKeySelect)
         buttonEncrypt.setOnClickListener(this::onButtonEncrypt)
@@ -107,7 +105,7 @@ class MainActivity : AppCompatActivity() {
         val intentKeyIdExtra = intent.getLongExtra(TextViewActivity.INTENT_EXTRA_KEY_ID, -1L)
 
         if (intentKeyIdExtra != -1L) {
-            onKeySelected(intentKeyIdExtra)
+            controller.currentKeyId = intentKeyIdExtra
             if (intentTextExtra != null) {
                 messageText.setText(intentTextExtra)
                 isTextEncrypted = false
@@ -121,29 +119,39 @@ class MainActivity : AppCompatActivity() {
             Intent.ACTION_VIEW -> {
                 val uri = intent.data
                 if (uri != null) {
-                    handleTextIntent(uri.toString())
+                    controller.decryptIntentText(uri.toString())
                 }
             }
             Intent.ACTION_SEND -> {
                 val text = intent.getStringExtra(android.content.Intent.EXTRA_TEXT)
                 if (text != null) {
-                    handleTextIntent(text)
+                    controller.decryptIntentText(text)
                 }
             }
             Intent.ACTION_PROCESS_TEXT ->
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     val text = intent.getCharSequenceExtra(android.content.Intent.EXTRA_PROCESS_TEXT)
                     if (text != null) {
-                        handleTextIntent(text.toString())
+                        controller.decryptIntentText(text.toString())
                     }
                 }
         }
     }
 
-    private fun onIntentMessageDecrypted(message: MessageBase): Boolean {
-        var success = false
+    override fun onIntentMessageDecryptResult(text: String, message: MessageBase?) {
 
         when (message) {
+            null -> {
+                // Decrypt failed
+                runOnUiThread {
+                    isTextEncrypted = false // don't allow sharing
+                    runOnUiThread {
+                        messageText.setText(text)
+                        Toast.makeText(this, R.string.text_didnt_match_keys, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
             is TextMessage -> {
                 runOnUiThread {
                     val intent = Intent(this, TextViewActivity::class.java)
@@ -152,25 +160,28 @@ class MainActivity : AppCompatActivity() {
                             .putExtra(TextViewActivity.INTENT_EXTRA_KEY_NAME, message.key.name)
                     startActivity(intent)
                 }
-                currentKey = message.key
-                success = true
             }
 
             is KeyReplacementMessage -> TODO("Not implemented")
 
             is KeyRevokeMessage -> TODO("Not implemented also")
         }
-
-        return success
     }
 
-    private fun onMessageDecrypted(message: MessageBase, updateKey: Boolean) {
+    override fun onMessageDecryptResult(text: String, message: MessageBase?) {
+
+        isTextEncrypted = message != null
+
         when (message) {
+            null ->  {
+                runOnUiThread {
+                    Toast.makeText(this, R.string.failed_to_decrypt, Toast.LENGTH_LONG).show()
+                }
+            }
+
             is TextMessage ->
                 runOnUiThread {
                     messageText.setText(message.text)
-                    if (updateKey)
-                        onKeySelected(message.key)
                 }
             is KeyReplacementMessage -> TODO("Not implemented")
 
@@ -178,59 +189,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun onMessageEncrypted(message: String) {
-        runOnUiThread {
-            messageText.setText(message)
-        }
-    }
-
-    private fun onDecryptFailed() {
-        runOnUiThread {
-            Toast.makeText(this, R.string.failed_to_decrypt, Toast.LENGTH_LONG).show()
-        }
-    }
-
-    private fun onEncryptFailed() {
-        runOnUiThread {
-            Toast.makeText(this, R.string.failed_to_encrypt, Toast.LENGTH_LONG).show()
-        }
-    }
-
-    private fun handleTextIntent(text: String) {
-
-        // Text can be either encrypted (user wants to decrypt it) or plaintext (user wants to
-        // encrypt it). Attempt to decrypt using known keys, if success - show the activity
-        // with the decrypted text, otherwise -- just populate text field here
-        background {
-            val keys = KeysDatabase(context = this).use { it.keys }
-            var success = false
-
-            val messageHandler = CryptoFactory.createMessageHandler()
-
-            val message = messageHandler.decrypt(text, keys)
-            if (message != null) {
-                success = onIntentMessageDecrypted(message)
+    override fun onMessageEncryptResult(message: String?) {
+        if (message != null) {
+            isTextEncrypted = true
+            runOnUiThread {
+                messageText.setText(message)
             }
-
-            if (!success) {
-                isTextEncrypted = false // don't allow sharing
-                runOnUiThread {
-                    messageText.setText(text)
-                    Toast.makeText(this, R.string.text_didnt_match_keys, Toast.LENGTH_LONG).show()
-                }
+        } else {
+            isTextEncrypted = false
+            runOnUiThread {
+                Toast.makeText(this, R.string.failed_to_encrypt, Toast.LENGTH_LONG).show()
             }
         }
     }
 
     private fun doSelectKeys(continueWithEncrypt: Boolean) {
-        val keys = KeysDatabase(context = this).use { it.keys }
-
         val builder = AlertDialog.Builder(this)
         builder.setIcon(android.R.drawable.ic_menu_directions)
         builder.setTitle(getString(R.string.select_a_key))
 
-        val names = keys.map { it.name }.toList() + listOf<String>(getString(R.string.text_password))
-        val values = keys.map { it.id }.toList() + listOf<Long>(-1L)
+        var (values, names) = controller.getKeyIdsWithNames()
+
+        names += listOf<String>(getString(R.string.text_password))
+        values += listOf(-1L)
 
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice, names)
 
@@ -242,7 +223,7 @@ class MainActivity : AppCompatActivity() {
             dialog, which ->
             if (which >= 0 && which < values.size && which < names.size) {
                 val keyId = values[which]
-                onKeySelected(keyId)
+                controller.currentKeyId = keyId
                 if (continueWithEncrypt) {
                     doEncrypt()
                 }
@@ -256,141 +237,77 @@ class MainActivity : AppCompatActivity() {
         doSelectKeys(false)
     }
 
-    private fun onKeySelected(keyId: Long) {
-        isKeyEverSelected = true
-        if (keyId != -1L) {
-            passwordText.visibility = View.GONE
-            currentKey = KeysDatabase(context = this).use { it.getKey(keyId) }
-            buttonKeySelect.text = getString(R.string.key_format).format(currentKey?.name)
-        }
-        else {
-            passwordText.visibility = View.VISIBLE
-            buttonKeySelect.text = getString(R.string.key_type_below)
-            currentKey = null
+    override fun onControllerKeySelected(key: KeyEntry?) {
+        runOnUiThread {
+            if (key != null) {
+                passwordText.visibility = View.GONE
+                buttonKeySelect.text = getString(R.string.key_format).format(key.name)
+            } else {
+                passwordText.visibility = View.VISIBLE
+                buttonKeySelect.text = getString(R.string.key_type_below)
+            }
         }
     }
 
-    private fun onKeySelected(key: KeyEntry?) {
-        isKeyEverSelected = true
-        if (key != null) {
-            passwordText.visibility = View.GONE
-            currentKey = key
-            buttonKeySelect.text = getString(R.string.key_format).format(currentKey?.name)
-        }
-        else {
-            passwordText.visibility = View.VISIBLE
-            buttonKeySelect.text = getString(R.string.key_type_below)
-            currentKey = null
+    override fun onEncryptPasswordIsEmpty() {
+        runOnUiThread {
+            Toast.makeText(this, R.string.password_is_empty, Toast.LENGTH_LONG).show()
         }
     }
 
+    override fun onEncryptPasswordIsInsecure(complexity: ExhaustiveSearchComplexity) {
 
-    private fun doEncrypt(key: KeyEntry, msg: String) {
-        try {
-            val encrypted = CryptoFactory.createMessageHandler().encrypt(TextMessage(key, msg), key)
-            isTextEncrypted = true
-            onMessageEncrypted(encrypted)
-        } catch (ex: Exception) {
-            onEncryptFailed()
+        runOnUiThread {
+            val builder = AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_launcher_foreground)
+                    .setTitle(getString(R.string.password_stength_notice))
+                    .setMessage(getString(R.string.still_proceed_question).format(
+                            complexity.formatPc(this),
+                            complexity.formatGpu(this),
+                            complexity.formatCluster(this)
+                    ))
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        controller.encrypt(
+                                messageText.text.toString(),
+                                passwordText.text.toString(),
+                                allowInsecurePassword = true
+                        )
+                    }
+                    .setNegativeButton(android.R.string.cancel) { _, _ -> }
+
+            builder.create().show()
         }
     }
 
     private fun doEncrypt() {
-        val msg = messageText.text.toString()
-        val password = passwordText.text.toString()
-
-        textViewError.visibility = View.GONE
-        val cKey = currentKey //  ?: CryptoFactory.deriveKeyFromPassword(key)
-
-        if (cKey == null) {
-            if (password.isEmpty()) {
-                Toast.makeText(this, R.string.password_is_empty, Toast.LENGTH_LONG).show()
-                return
-            }
-
-            val complexity = PasswordComplexityEstimator.getExhaustiveSearchComplexity(password)
-            if (!complexity.isSecure) {
-                val builder = AlertDialog.Builder(this)
-                        .setIcon(R.drawable.ic_launcher_foreground)
-                        .setTitle(getString(R.string.password_stength_notice))
-                        .setMessage(getString(R.string.still_proceed_question).format(
-                                complexity.formatPc(this),
-                                complexity.formatGpu(this),
-                                complexity.formatCluster(this)
-                        ))
-                        .setPositiveButton(android.R.string.ok) { _, _ ->
-                            pleaseWaitText.visibility = View.VISIBLE
-                            background {
-                                val key = CryptoFactory.deriveKeyFromPassword(password, "")
-                                runOnUiThread { pleaseWaitText.visibility = View.GONE }
-                                doEncrypt(key, msg)
-                            }
-                        }
-                        .setNegativeButton(android.R.string.cancel) { _, _ -> }
-
-                builder.create().show()
-                return
-            }
-
-            pleaseWaitText.visibility = View.VISIBLE
-        }
-
-        background {
-            val key = cKey ?: CryptoFactory.deriveKeyFromPassword(password, "")
-            runOnUiThread { pleaseWaitText.visibility = View.GONE }
-            doEncrypt(key, msg)
-        }
+        controller.encrypt(
+                messageText.text.toString(),
+                passwordText.text.toString()
+        )
     }
 
     private fun onButtonEncrypt(v: View) {
-        if (!isKeyEverSelected)
+        if (!controller.isKeyEverSelected)
             doSelectKeys(continueWithEncrypt = true)
         else
             doEncrypt()
     }
 
-    private fun decryptIterateAllKeys(text: String) {
-
-        val keys = KeysDatabase(context = this).use { it.keys }
-
-        val messageHandler = CryptoFactory.createMessageHandler()
-
-        val message = messageHandler.decrypt(text, keys)
-        if (message != null) {
-            onMessageDecrypted(message, true)
-        }
-        else {
-            onDecryptFailed()
-        }
+    override fun onKeyDerivationStarted() {
+        runOnUiThread { pleaseWaitText.visibility = View.VISIBLE }
     }
 
+    override fun onKeyDerivationFinished() {
+        runOnUiThread { pleaseWaitText.visibility = View.GONE }
+    }
+
+
+
     private fun onButtonDecrypt(v: View) {
-        val msg = messageText.text.toString()
-        val password = passwordText.text.toString()
-
-        textViewError.visibility = View.GONE
-
-        if (currentKey == null)
-            pleaseWaitText.visibility = View.VISIBLE
-
-        background {
-            try {
-                val cKey = currentKey ?: CryptoFactory.deriveKeyFromPassword(password, "")
-                runOnUiThread { pleaseWaitText.visibility = View.GONE }
-
-                val decrypted = CryptoFactory.createMessageHandler().decrypt(msg, cKey)
-
-                if (decrypted != null) {
-                    isTextEncrypted = false
-                    onMessageDecrypted(decrypted, false)
-                }
-                else {
-                    decryptIterateAllKeys(msg)
-                }
-            } catch (ex: Exception) {
-                decryptIterateAllKeys(msg)
-            }
-        }
+        controller.decrypt(
+                messageText.text.toString(),
+                passwordText.text.toString()
+        )
     }
 
     private fun copyText() {
---
+++ app/src/main/java/net/cxifri/ui/MainActivityController.kt
@@ -0,0 +1,145 @@
+package net.cxifri.ui
+
+import android.content.Context
+import android.widget.Toast
+import net.cxifri.R.id.message
+import net.cxifri.R.id.password
+import net.cxifri.crypto.*
+import net.cxifri.keysdb.KeysDatabase
+import net.cxifri.utils.ExhaustiveSearchComplexity
+import net.cxifri.utils.PasswordComplexityEstimator
+import net.cxifri.utils.background
+
+interface MainView  {
+    fun onControllerKeySelected(key: KeyEntry?)
+
+    fun onKeyDerivationStarted()
+    fun onKeyDerivationFinished()
+
+    fun onEncryptPasswordIsEmpty()
+    fun onEncryptPasswordIsInsecure(complexity: ExhaustiveSearchComplexity)
+
+    fun onMessageEncryptResult(encrypted: String?)
+    fun onIntentMessageDecryptResult(text: String, message: MessageBase?)
+    fun onMessageDecryptResult(text: String, msg: MessageBase?)
+}
+
+class MainActivityController(val context: Context, val view: MainView) {
+
+    private var _currentKey: KeyEntry? = null
+    private var _isKeyEverSelected = false
+
+    val isKeyEverSelected get() = _isKeyEverSelected
+
+    private val messageHandler: MessageHandlerInterface by lazy { CryptoFactory.createMessageHandler() }
+
+    var currentKey: KeyEntry?
+        get() {
+            return _currentKey
+        }
+        set(value) {
+            _isKeyEverSelected = true
+            _currentKey = value
+            view.onControllerKeySelected(_currentKey)
+        }
+
+    var currentKeyId: Long
+        get() {
+            return _currentKey?.id ?: -1L
+        }
+        set(value) {
+            _currentKey = KeysDatabase(context).use { it.getKey(value) }
+            view.onControllerKeySelected(_currentKey)
+        }
+
+    fun encrypt(msg: String, passwordForEmptyKey: String?, allowInsecurePassword: Boolean = false) {
+
+        background {
+            var key = _currentKey
+
+            if (key == null) {
+                if (passwordForEmptyKey == null || passwordForEmptyKey.isEmpty()) {
+                    view.onEncryptPasswordIsEmpty()
+                    return@background
+                }
+
+                if (!allowInsecurePassword) {
+                    val complexity = PasswordComplexityEstimator.getExhaustiveSearchComplexity(passwordForEmptyKey)
+                    if (!complexity.isSecure) {
+                        view.onEncryptPasswordIsInsecure(complexity)
+                        return@background
+                    }
+                }
+
+                view.onKeyDerivationStarted()
+
+                key = CryptoFactory.deriveKeyFromPassword(
+                        password = passwordForEmptyKey,
+                        name = "")
+
+                view.onKeyDerivationFinished()
+            }
+
+            val encrypted =
+                    try {
+                        messageHandler.encrypt(TextMessage(key, msg), key)
+                    } catch (ex: Exception) {
+                        null
+                    }
+            view.onMessageEncryptResult(encrypted)
+        }
+    }
+
+    private fun decryptIterateAll(text: String, onResult: (MessageBase?) -> Unit) {
+
+        val keys = KeysDatabase(context).use { it.keys }
+
+        val message = messageHandler.decrypt(text, keys)
+        if (message != null && message.key.id != currentKeyId) {
+            currentKey = message.key
+        }
+
+        onResult(message)
+    }
+
+    fun decryptIntentText(text: String) {
+        background {
+            decryptIterateAll(text) { msg -> view.onIntentMessageDecryptResult(text, msg) }
+        }
+    }
+
+    fun decrypt(text: String, passwordForEmptyKey: String) {
+
+        background {
+            var key = _currentKey
+
+            if (key == null) {
+                view.onKeyDerivationStarted()
+                key = CryptoFactory.deriveKeyFromPassword(
+                        password = passwordForEmptyKey,
+                        name = "")
+                view.onKeyDerivationFinished()
+            }
+
+            try {
+                val decrypted = messageHandler.decrypt(text, key)
+                if (decrypted != null) {
+                    view.onMessageDecryptResult(text, decrypted)
+                } else {
+                    decryptIterateAll(text) { msg -> view.onMessageDecryptResult(text, msg) }
+                }
+            } catch (ex: Exception) {
+                decryptIterateAll(text) { msg -> view.onMessageDecryptResult(text, msg) }
+            }
+        }
+    }
+
+    fun getKeyIdsWithNames(): Pair<List<Long>, List<String>> {
+        val keys = KeysDatabase(context).use { it.keys }
+
+        val names = keys.map { it.name }.toList()
+        val values = keys.map { it.id }.toList()
+
+        return Pair(values, names)
+    }
+}
